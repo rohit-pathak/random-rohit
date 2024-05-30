1110f0d46e87a2270f9b9c52851fb49a9d41aca4
Rohit Pathak <rohitpathak.3@gmail.com>
2024-05-30 12:15:56 -0400

    create skeleton aid-data-viz component

---
+++ src/app/aid-data-viz/aid-data-viz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aid-data-viz',
+  standalone: true,
+  imports: [],
+  templateUrl: './aid-data-viz.component.html',
+  styleUrl: './aid-data-viz.component.scss'
+})
+export class AidDataVizComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { AidDataVizComponent } from "./aid-data-viz/aid-data-viz.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'aid-data', component: AidDataVizComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
