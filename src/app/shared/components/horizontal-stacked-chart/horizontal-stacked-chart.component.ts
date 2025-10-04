1df053f1f2d003be756d9e3c802cb2bd6d1d3310
Rohit Pathak <rohitpathak.3@gmail.com>
2025-10-04 16:27:27 -0400

    feat(aid-data): create shared stacked bar chart component

---
+++ src/app/shared/components/horizontal-stacked-chart/horizontal-stacked-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-stacked-chart',
+  imports: [],
+  templateUrl: './horizontal-stacked-chart.component.html',
+  styleUrl: './horizontal-stacked-chart.component.scss'
+})
+export class HorizontalStackedChartComponent {
+
+}
