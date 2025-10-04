9367db5dfd6b6493e6b2f92d9cf4b5028727a147
Rohit Pathak <rohitpathak.3@gmail.com>
2025-10-03 23:38:44 -0400

    feat(aid-data): use shared line chart for total transactions component

--- src/app/aid-data-viz/total-transactions-line-chart/total-transactions-line-chart.component.ts
+++ src/app/aid-data-viz/total-transactions-line-chart/total-transactions-line-chart.component.ts
@@ -1,111 +1,52 @@
-import { AfterViewInit, Component, computed, effect, ElementRef, inject, Injector, viewChild } from '@angular/core';
-import { axisBottom, brushX, D3BrushEvent, line, max, min, scaleLinear, scaleLog, select } from "d3";
+import { Component, computed, inject } from '@angular/core';
 import { AidDataStore, YearTotal } from "../aid-data.store";
 import { ResizeDirective } from "../../shared/directives/resize.directive";
+import {
+  BrushSpan,
+  LineData,
+  MultiLineChartComponent
+} from "../../shared/components/multi-line-chart/multi-line-chart.component";
 
 @Component({
   selector: 'app-total-transactions-line-chart',
-  imports: [],
+  imports: [
+    MultiLineChartComponent
+  ],
   templateUrl: './total-transactions-line-chart.component.html',
   styleUrl: './total-transactions-line-chart.component.scss',
   hostDirectives: [ResizeDirective],
 })
-export class TotalTransactionsLineChartComponent implements AfterViewInit {
-  private readonly injector = inject(Injector);
-  private readonly svgRef = viewChild.required<ElementRef>('lineChart');
-  private readonly lineRef = viewChild.required<ElementRef>('line');
-  private readonly xAxisRef = viewChild.required<ElementRef>('xAxis');
-  private readonly svg = computed(() => select<SVGSVGElement, unknown>(this.svgRef().nativeElement));
-  private readonly lineGroup = computed(() => select<SVGGElement, unknown>(this.lineRef().nativeElement));
-  private readonly xAxisGroup = computed(() => select<SVGGElement, unknown>(this.xAxisRef().nativeElement));
-  private readonly resize = inject(ResizeDirective);
-  private readonly aidDataStore = inject(AidDataStore);
-
-  private readonly padding = {top: 5, bottom: 20};
-  protected readonly totalHeight = 100;
-  protected readonly dimensions= this.resize.dimensions;
-  protected readonly height = this.totalHeight - this.padding.top - this.padding.bottom;
-  private readonly xScale = computed(() => {
-    const data = this.aidDataStore.transactionsPerYear();
-    const width = this.dimensions().width;
-    // TODO: use totalYearRange from store
-    return scaleLinear([min(data.map(d => d.year)) ?? 0, max(data.map(d => d.year)) ?? 0], [0, width]);
-  })
-  private readonly yScale = computed(() => {
-    const data = this.aidDataStore.transactionsPerYear();
-    const amounts = data.map(d => d.amount);
-    return scaleLog([min(amounts) ?? 0, max(amounts ?? 0) ?? 0], [this.height, this.padding.top]);
+export class TotalTransactionsLineChartComponent {
+  private readonly store = inject(AidDataStore);
+  protected readonly lines = computed<LineData<YearTotal>[]>(() => {
+    return [
+      {
+        name: 'Total transactions',
+        data: this.store.transactionsPerYear()
+      },
+    ];
   });
-  private readonly lineGenerator = computed(() => {
-    return line<YearTotal>()
-      .x(d => this.xScale()(d.year) ?? 0)
-      .y(d => this.yScale()(d.amount) ?? 0);
-  });
-  private readonly xAxisGenerator = computed(() => axisBottom(this.xScale()));
-  private readonly chartBrush = brushX().on('brush end', (e) => this.handleBrush(e));
+  protected readonly x = (d: YearTotal) => d.year;
+  protected readonly y = (d: YearTotal) => d.amount;
+  protected readonly xSpan = this.store.totalYearRange;
+  protected readonly brushSpan = this.store.brushSpan;
 
-  // TODO: doesn't need to be computed
-  protected readonly axisTransform = computed(() => `translate(0, ${this.padding.top + this.height})`);
-
-  public ngAfterViewInit(): void {
-    this.initializeDrawing();
-  }
-
-  private initializeDrawing(): void {
-    effect(() => {
-      const dimensions = this.dimensions();
-      const data = this.aidDataStore.transactionsPerYear();
-      if (!dimensions || !data) {
-        return;
-      }
-      this.drawLineChart();
-      this.drawXAxis();
-      this.addBrush();
-    }, {injector: this.injector});
-  }
-
-  private drawLineChart(): void {
-    const data = this.aidDataStore.transactionsPerYear();
-    this.lineGroup()
-      .selectAll('path')
-      .data([data])
-      .join('path')
-      .attr('d', d => this.lineGenerator()(d))
-      .attr('fill', 'none')
-      .attr('stroke', '#bbb');
-  }
-
-  private drawXAxis(): void {
-    this.xAxisGroup().call(this.xAxisGenerator());
-  }
-
-  private addBrush(): void {
-    if (!this.lineGroup().select('.overlay').empty()) {
-      this.lineGroup().call(this.chartBrush.move, null);
-    }
-    // TODO: check why after resize the brush area is not correct
-    this.lineGroup().call(this.chartBrush);
-  }
-
-  private handleBrush(e: D3BrushEvent<unknown>): void {
-    this.aidDataStore.setBrushSpan(e.selection as [number, number] | null);
-    // TODO: remove, compute year range based on brushSpan
-    if (!e.selection) {
-      this.aidDataStore.setYearRange(null);
+  onBrush(span: BrushSpan | null): void  {
+    if (!span) {
+      this.store.setBrushSpan(null);
+      this.store.setYearRange(null);
       return;
     }
-    const [x1, x2] = e.selection as [number, number];
-    const selectedYears = this.aidDataStore.transactionsPerYear()
+    this.store.setBrushSpan(span?.range ?? null);
+    const [start, end] = span.domain;
+    const selectedYears = this.store.transactionsPerYear()
       .map(t => t.year)
-      .filter(year => (this.xScale()(year) >= x1) && (this.xScale()(year) <= x2));
+      .filter(year => (year >= start) && (year <= end));
     if (!selectedYears.length) {
-      this.aidDataStore.setYearRange(null);
-      if (e.type === 'end') {
-        this.lineGroup().call(this.chartBrush.move, null);
-      }
-      return;
+      this.store.setYearRange(null);
+    } else {
+      this.store.setYearRange([selectedYears[0], selectedYears.at(-1)!]);
     }
-    this.aidDataStore.setYearRange([selectedYears[0], selectedYears.at(-1)!]);
   }
 
 }
